8ec4a270ef40191e8b24bed853ac75e6183657e8
unknown <alvito_rifky_29rpl@student.smktelkom-mlg.sch.id>
2021-01-15 00:28:28 +0700

    Initial Commit

---
+++ src/modul/pkg1/lanjut/praktikum3/praktikum3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul.pkg1.lanjut.praktikum3;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class praktikum3 {
+      public static void main(String[] args) {
+    int i;
+        for (i = 0; i < 2; i++) {
+            System.out.println("");
+        }//spasi 
+        int zx = 4;
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+    }
+}
